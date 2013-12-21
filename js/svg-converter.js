01f0fc9af4008d9c0b9efe4617bab9a8356ae7c1
Maruf Morshed <me@marufm.com>
2013-12-22 00:43:02 +1100

    form elements

--- js/svg-converter.js
+++ js/svg-converter.js
@@ -33,6 +33,14 @@ $('document').ready(function(){
 });
 
 var svgconverter = {
+
+    svgs: {
+        svg_ids: [],
+        svg_data: [],
+        add_svg: function(){
+
+        }
+    },
     
     init: function() {
         
@@ -85,51 +93,114 @@ var svgconverter = {
                     reader.onload = (function(theFile) {
                         return function(e) {
                             var id = theFile.name.substr(0, theFile.name.lastIndexOf('.'));
-                            svgconverter.addInfo(e.target.result, id);
+                            svgconverter.make_svg_element(e.target.result, id);
+                            console.log('here');
                         };
                     })(f);
                     // Read in the image file as a data URL.
                     reader.readAsText(f);
                 }
+
+
+
             });
             
+            $( "#options-form" ).on( "submit", function( event ) {
+                event.preventDefault();
+                  //console.log( $( this ).serializeArray() );
+
+                var optionfields =  $( this ).serializeArray();
+
+                var encoding_to_use = "",
+                    format_to_use = "";
+                
+                jQuery.each( optionfields, function( i, field ) {
+                    if(field.name === 'encoding'){
+                        encoding_to_use = field.value;
+                    }
+                    if(field.name === 'format'){
+                        format_to_use = field.value;
+                    }
+                });
+
+                switch (encoding_to_use){
+                    case "utf8":
+                        console.log('Making utf8 encoded code');
+                        break;
+                    case "uri":
+                        console.log('Making uri encoded code');
+                        break;
+                    case "b64":
+                        console.log('Making base64 encoded code');
+                        break;
+                }
+
+                switch (format_to_use){
+                    case "css":
+                        console.log('Making utf8 encoded code');
+                        break;
+                    case "scss":
+                        console.log('Making uri encoded code');
+                        break;
+                }
+
+            });
         
     },
     
-    addInfo: function( data, id ) {
-        data = encodeURIComponent(data);
-        data = data.replace(/\(/g,'%28').replace(/\)/g,'%29');
-        var element = $('#' + id);
+    make_svg_element: function( svg_data, svg_name ) {
+        
+        // Create Dom Element Variable
+        var dom_element = $('<div />', {id: svg_name,});
+        dom_element.data("raw_svg_data", svg_data);   // add svg data to dom element
+        dom_element.data("file_name", svg_name);    // add svg name to dom element
+
+
+
+        // var dom_element = $('<div />', {id: svg_name,});
+        post_svg_data = encodeURIComponent(svg_data);
+        post_svg_data = post_svg_data.replace(/\(/g,'%28').replace(/\)/g,'%29');
+        
+
+        // check to see if this svg already exists by doing a dom search
+        // length should be greather than 0;
+        var element = $('#' + svg_name);
         if( element.length == 0 ) {
-            svgconverter.createIconDom(data, id);
+            
+            var icon = $('<div />', {class: "svg-icon",});
+            icon.css(svgconverter.create_css_str(post_svg_data));
+            var button = $('<a title="Remove Icon" class="remove-icon" href="#!"></a>').click(function(){
+                $(this).parent().parent().remove();
+            })
+            var fname = $('<div class="filename"><p>' + svg_name + '</p></div>');
+            icon.appendTo(dom_element);
+            fname.appendTo(dom_element);
+            button.appendTo(dom_element);
+            dom_element.appendTo('#data');
+            //svgconverter.createIconDom(data, id);
         } else {
-            svgconverter.updateDomIcon(data, id);
+            $( '#' + svg_name + '.svg-icon').css( svgconverter.create_css_str( post_svg_data ) );
         }
     },
-    
+
     createIconDom: function( data, id ) {
-        var iconWrapper = $('<div id="' + id + '" class="icon-info g-block"/>');
-        var icon = $('<div class="svg-icon"/>');
-        icon.css(svgconverter.create_css_str(data));
-        var button = $('<a title="Remove Icon" class="remove-icon" href="#!"></a>').click(function(){
-            $(this).parent().parent().remove();
-        })
-        var fname = $('<div class="filename"><p>' + id + '</p></div>');
-        var input_svg = $('<input type="hidden" name="svg_data[]" value="' + data + '" />');
-        var input_name = $('<input type="hidden" name="svg_name[]" value="' + id + '" />');
-        icon.append(button)
+        var iconWrapper = $('<div id="' + id + '" class="icon-info"/>');
+        
+       
+       
+        //var input_svg = $('<input type="hidden" name="svg_data[]" value="' + data + '" />');
+        // var input_name = $('<input type="hidden" name="svg_name[]" value="' + id + '" />');
+        icon.append(button);
         icon.append(fname);
-        icon.append(input_svg)
-        icon.append(input_name)
 
         iconWrapper.append(icon);
         $('#data').append(iconWrapper);
 
-        var svgc_string = '<div class="svg-icon-tile"><div class="svg-icon-filename">' + id + '</div><div class="svg-icon ' + id + '"></div></div>';
-        var svgc_html = $('<div />').text(svgc_string).html();
-        var svgc_css = '.svg-icon.' + id + '{ \n background-image:url("data:image/svg+xml,' + data + '"); \n .no-svg & { background-image:url(../images/' + id +'.png);} \n}';
+        // var svgc_string = '<div class="svg-icon-tile"><div class="svg-icon-filename">' + id + '</div><div class="svg-icon ' + id + '"></div></div>';
+        // var svgc_html = $('<div />').text(svgc_string).html();
+        // var svgc_css = '.svg-icon.' + id + '{ \n background-image:url("data:image/svg+xml,' + data + '"); \n .no-svg & { background-image:url(../images/' + id +'.png);} \n}';
 
-        console.log(svgc_css);
+        
 
         // $('#digityle-html').append(svgc_html + '\n');
         // $('#digityle-css').append(svgc_css + '\n');
@@ -146,10 +217,7 @@ var svgconverter = {
     },
 
     spitOutCss: function (){
-        $('#get_css_btn').on('click', function(){
 
-        });
-        
     },
 
     getCssOptions: function(){
