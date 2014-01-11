f553c6a6715c0d70c8c518ff911437ec7f9560c1
Maruf Morshed <me@marufm.com>
2014-01-12 00:05:19 +1100

    encoding option, svg rendering onto page

--- js/svg-converter.js
+++ js/svg-converter.js
@@ -103,7 +103,7 @@ var svgconverter = {
     make_svg_element: function( svg_data, svg_name ) {
         
         // Create Dom Element Variable
-        var dom_element = $('<div />', {id: 'svg_name',class: 'svg-icon'});
+        var dom_element = $('<div />', {id: svg_name,class: 'svg-icon-shell'});
         dom_element.data("raw_svg_data", svg_data);   // add svg data to dom element
         dom_element.data("file_name", svg_name);    // add svg name to dom element
 
@@ -118,19 +118,21 @@ var svgconverter = {
         var element_check = $('#' + svg_name);
         if( element_check.length == 0 ) {
             
-            var icon = $('<div />', {class: "svg-icon-shell",});
+            var icon = $('<div />', {class: "svg-icon",});
             //icon.css(svgconverter.create_css_str(post_svg_data));
             var button = $('<a title="Remove Icon" class="remove-icon" href="#!"></a>').click(function(){
                 $(this).parent().parent().remove();
             })
             var fname = $('<div class="filename"><p>' + svg_name + '</p></div>');
+            var make_str = svgconverter.create_css_str(svg_data,"<uri></uri>");
+            icon.css(svgconverter.create_css_str(svg_data, "uri", "object"));
             icon.appendTo(dom_element);
             fname.appendTo(dom_element);
             button.appendTo(dom_element);
             dom_element.appendTo('#data');
             //svgconverter.createIconDom(data, id);
         } else {
-            $( '#' + svg_name + '.svg-icon').css( svgconverter.create_css_str( post_svg_data ) );
+            $( '#' + svg_name + '.svg-icon-shell').css( svgconverter.create_css_str( svg_data, "uri", "object") );
         }
     },
 
@@ -159,61 +161,75 @@ var svgconverter = {
     },
     
     updateDomIcon: function( data, id ) {
-        $( '#' + id + ' .svg-icon' ).css( this.create_css_str( data ) );
+        $( '#' + id + ' .svg-icon' ).css( this.create_css_str( data, "uri" ) );
         $( '#' + id + ' input' ).attr( 'value', data );
     },
     
-    create_css_str: function( data ) {
-        return {'background-image' : 'url("data:image/svg+xml,' + data + '")'};
+    create_css_str: function( data , encoding, type) {
+
+        var css_str;
+
+        if(encoding == "uri"){
+            
+            css_str = 'url("data:image/svg+xml,' + svgconverter.create_escaped_data(data, "uri") + '")';
+            console.log(svgconverter.create_escaped_data(data, "uri"));
+        }else if(encoding == "b64"){
+            css_str = 'url("data:image/svg+xml;base64,' + svgconverter.create_escaped_data(data, "uri") + '")';
+        }
+
+        //return {'background-image' : 'url("data:image/svg+xml,' + data + '")'};
+        
+        if(type == "object"){
+            return {'background-image' : css_str};
+        }else{
+            return "background-image: " + css_str + ';'
+        }
+        
     },
 
-    create_escaped_data: function (data, encoding_to_use){
+    create_escaped_data: function (raw_data, encoding_to_use){
+        var data_string,
+            processed_svg_data;
 
+        if(encoding_to_use == "uri"){
+                processed_svg_data = encodeURIComponent(raw_data);
+                processed_svg_data = processed_svg_data .replace(/\(/g,'%28').replace(/\)/g,'%29');
+        }else if(encoding_to_use == "b64"){
+                processed_svg_data = btoa(raw_data);
+        }
+
+        return processed_svg_data;
     },
 
     spitOutCss: function (encoding, format){
 
-        var svgs = $('#data > .svg-icon').each(function(index){
-            
-        });
-
         var encoding_to_use, format_to_use;
 
         var output_text_area = $('#output');
+        output_text_area.val("");
 
-        var svgs = $('#data > .svg-icon').each(function(index){
+        var svgs = $('#data > .svg-icon-shell').each(function(index){
             var raw_svg_data = $(this).data('raw_svg_data'),
                 svg_name = $(this).data('file_name'),
                 processed_svg_data,
                 final_string;
 
-                
-
-            switch (encoding){
-                case "utf8":
-                    processed_svg_data = raw_svg_data;
-                    break;
-                case "uri":
-                    processed_svg_data = encodeURIComponent(raw_svg_data);
-                    processed_svg_data = processed_svg_data .replace(/\(/g,'%28').replace(/\)/g,'%29');
-                    break;
-                case "b64":
-                    processed_svg_data = window.atob(raw_svg_data);
-                    processed_svg_data = ';base64, ' + processed_svg_data;
-                    break;
-            }
+            processed_svg_data = svgconverter.create_escaped_data(raw_svg_data, encoding);
 
             switch (format){
                 case "css":
-                    final_string = '.' + svg_name + '{background-image:url("data:image/svg+xml,' + processed_svg_data + '");}';
+                    final_string = '.' + svg_name + '{background-image:url("data:image/svg+xml,' + processed_svg_data + '");}\n';
+                    final_string += '.no-svg .' + svg_name + '{background-image:url(../PNG_DIRECTORY/'+ svg_name +'.png)}'; 
                     break;
                 case "scss":
-                    final_string = '@' + svg_name + '{background-image:url("data:image/svg+xml,' + processed_svg_data + '");}';
+                    final_string = '.' + svg_name + '{background-image:url("data:image/svg+xml,' + processed_svg_data + '");}';
+                    break;
+                case "less":
+                    final_string = '.' + svg_name + '{background-image:url("data:image/svg+xml,' + processed_svg_data + '");}';
                     break;
             }
 
             output_text_area.val( output_text_area.val() + '\n\n' + final_string);
-
         });
 
 
